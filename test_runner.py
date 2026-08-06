db05585db836b76bbcfc34db9a6102215f9077b4
Nicolas <nicolasmader001@gmail.com>
2026-08-06 15:17:55 +0200

    feat(test): add url status check

--- test_runner.py
+++ test_runner.py
@@ -1,3 +1,8 @@
+import  requests                        #Requests zum Prüfen von Webseiten und APIs per HTTP-Anfragen,urls prüfen und Serverantworten auszulesen
+
+
 def run_test(url):
-    print("Starte Test...")
-    print("Teste:", url)
+    response = requests.get(url)
+
+    print("URL:", url)
+    print("Status:", response.status_code)
