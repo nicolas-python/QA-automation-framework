12ec7bd95c5c7eaf8c837ce374dc225ba1865544
Nicolas <nicolasmader001@gmail.com>
2026-08-06 15:29:53 +0200

    feat(test): add response time check

--- test_runner.py
+++ test_runner.py
@@ -1,8 +1,13 @@
 import  requests                        #Requests zum Prüfen von Webseiten und APIs per HTTP-Anfragen,urls prüfen und Serverantworten auszulesen
+import time
 
 
 def run_test(url):
+    start = time.time()                 #time.time() aus dem time Modul gibt die aktuelle Zeit in Sekunden zurück.
     response = requests.get(url)
+
+    end = time.time()
 
     print("URL:", url)
     print("Status:", response.status_code)
+    print("Antwortzeit:", round(end - start, 2), "Sekunden")                # round(..., 2) rundet auf 2 Nachkommastellen
